5db57bb29db8308107bb00a243c4fda862b6dd07
Alison Golden <alisong779@yahoo.com>
2019-05-14 15:40:36 +0000

    add scrape data

--- lib/astro-cli/scraper.rb
+++ lib/astro-cli/scraper.rb
@@ -1,6 +1,17 @@
 class Scraper 
   
-  html = open("https://www.astrology.com/astrology-101")
-  doc = Nokogiri::HTML(html)
-  binding.pry 
+  def scrape_signs
+    html = open("https://www.astrology.com/astrology-101")
+    doc = Nokogiri::HTML(html)
+    binding.pry 
+  end 
+  
+  #Signs:
+  #Aries = doc.css("icon-aries-symbol").text 
+ 
+  
+  def scrape_traits
+    html = open(https://www.astrology.com/astrology-101/zodiac-signs/aries)
+    doc = Nokogiri::HTML(html)
+ 
 end 
