2307733402581adb3ae6155731c65d37ec8897b9
Alison Golden <alisong779@yahoo.com>
2019-05-15 20:03:04 +0000

    add format

--- lib/astro-cli/cli.rb
+++ lib/astro-cli/cli.rb
@@ -1,26 +1,25 @@
 class CLI  
  
   def run 
-      binding.pry 
-      input = ""
-      puts "Hello There! Welcome to your Astrology Source!"
-      Scraper.scrape_signs_and_planets
-      puts "To find out what your Astrological Sign is, enter 'sign'."
-      puts "To learn about the specific traits of your Astrological Sign, enter 'traits'."
-      puts "To learn about the planet that rules your life, type 'planet'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
+    input = ""
+    puts "Hello There! Welcome to your Astrology Source!"
+    Scraper.scrape_signs_and_planets
+    puts "To find out what your Astrological Sign is, enter 'sign'."
+    puts "To learn about the specific traits of your Astrological Sign, enter 'traits'."
+    puts "To learn about the planet that rules your life, type 'planet'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
   
-      until input == "exit"
-        input = gets.strip
-        if input == "sign"
-          astro_signs
-        elsif input == "traits"
-          astro_traits
-        elsif input == "planet"
-          astro_planets
-        else
-          puts "Please enter a valid choice:" 
+    until input == "exit"
+      input = gets.strip
+      if input == "sign"
+        astro_signs
+      elsif input == "traits"
+        astro_traits
+      elsif input == "planet"
+        astro_planets
+      else
+        puts "Please enter a valid choice:" 
       end 
     end 
   end 
--- lib/astro-cli/scraper.rb
+++ lib/astro-cli/scraper.rb
@@ -8,23 +8,24 @@ class Scraper
       doc.css(".zodiac-101-links")[0].css(".grid-new.grid-4")[0].css("a").each do |sign|
         new_sign = Sign.new 
         new_sign.name = sign.text.strip 
-        new_sign.url = sign.href
+        new_sign.url = " "
       end 
       
       doc.css(".zodiac-101-links")[0].css(".grid-new.grid-4")[1].css("a").each do |planet|
         new_planet = Planet.new 
         new_planet.name = planet.text.strip
-        new_planet.url = sign.href
+        new_planet.url = " "
+          binding.pry 
       end 
   end 
   
-  def self.scrape_sign_traits(#sign)
-  #sign.url
-  #scrape and add to object
-      #.css(“.body”)[0].css(“p”)[2].text
-  end
+  # def self.scrape_sign_traits(#sign)
+  # #sign.url
+  # #scrape and add to object
+  #     #.css(“.body”)[0].css(“p”)[2].text
+  # end
     
-  def self.scrape_planet_deets(#planet)
-      #.css(".body").css("p")[0..10].text 
-  end 
+  # def self.scrape_planet_deets(#planet)
+  #     #.css(".body").css("p")[0..10].text 
+  # end 
 end 
